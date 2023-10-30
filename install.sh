1ff8578d43ce070c2da6f420b508c32cb8ad5d54
omores <124463839+omores@users.noreply.github.com>
2023-10-30 18:30:37 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+yay -S hyprland eww-wayland ttf-ubuntu-nerd socat jq acpi inotify-tools\
+  bluez pavucontrol brightnessctl playerctl nm-connection-editor imagemagick \
+  gjs gnome-bluetooth-3.0 upower networkmanager gtk3 \
+  wl-gammactl wlsunset wl-clipboard hyprpicker hyprshot blueberry \
+  polkit-gnome
